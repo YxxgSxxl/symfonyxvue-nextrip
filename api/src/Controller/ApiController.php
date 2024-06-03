0328d7a41f854909f440286337f071badade3907
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-06-03 16:59:49 +0200

    started php fetch for openweather API

--- api/src/Controller/ApiController.php
+++ api/src/Controller/ApiController.php
@@ -12,13 +12,13 @@ class ApiController extends AbstractController
     #[Route('/api/{city1}/{city2}', name: 'app_api')]
     public function index(string $city1, string $city2): JsonResponse
     {
-        $response = new Response();
 
         // code
 
-        // $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response = new Response();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'winner' => 'The winner is PutACityHere !',
             'path' => 'src/Controller/ApiController.php',
         ]);
     }
