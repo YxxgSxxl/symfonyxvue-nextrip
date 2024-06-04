4ae89a2c11a89e3fd71a11927d8f4ca04fa1ab65
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-06-04 11:14:30 +0200

    CORD fixed

--- api/src/Controller/ApiController.php
+++ api/src/Controller/ApiController.php
@@ -10,16 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class ApiController extends AbstractController
 {
     #[Route('/api/{city1}/{city2}', name: 'app_api')]
-    public function index(string $city1, string $city2): JsonResponse
+    public function compare(string $city1, string $city2): JsonResponse
     {
         // FETCH OpenWeather map API here
         $url_base = "https://api.openweathermap.org/data/2.5/";
         $api_key = "95542917d76459372397547a96610cd8";
 
-        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            http_response_code(200);
-            exit;
-        }
+        // if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        //     http_response_code(200);
+        //     exit;
+        // }
 
         // $query1 = new Response();
         $query1 = file_get_contents($url_base . "weather?q=" . $city1 . "&units=metric&appid=" . $api_key, true);
@@ -28,7 +28,7 @@ class ApiController extends AbstractController
         // $query2->headers->set('Access-Control-Allow-Origin', '*');
         // dd($query1, $query2);
 
-        $response = new Response();
+        // $response = new Response();
         // $response->headers->set('Access-Control-Allow-Origin', '*');
         return $this->json([
             'city1' => $query1,
