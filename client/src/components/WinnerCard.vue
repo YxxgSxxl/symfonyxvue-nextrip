baf0803822e9a1e1cdb6e055d34e8a7a0a228a52
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-06-06 16:38:18 +0200

    vue conffeti added + winner comp

---
+++ client/src/components/WinnerCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps({
+    winnerData: Object,
+});
+</script>
+
+<template>
+    {{ props.winnerData?.city1 }}
+
+    <div class="winnerCard">
+        <div class="winnercard-name">
+            <img src="../../public/crown.svg" alt="Winner Crown" width="50">
+            <h2>{{ props.winnerData?.city1.name }}</h2>
+        </div>
+        azfza
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.winnercard {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+
+    &-name {
+        text-align: center;
+    }
+}
+</style>
--- client/src/view/TripView.vue
+++ client/src/view/TripView.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue' // Refs
 import axios from 'axios' // Axios
+import ConfettiExplosion from "vue-confetti-explosion" // Vue Conffeti Animations lib
 import TripForm from '../components/TripForm.vue' // Trip Form component
 import TripCard from '../components/TripCard.vue' // Trip Card component
+import WinnerCard from '../components/WinnerCard.vue' // Win Card component
 
-let title = ref("Compare two cities")
-let isLoading: any = ref(false)
-let search: any = ref(true)
-let searched: any = ref(false)
-let error: any = ref("")
-let data_weather: any = ref([]);
+let title = ref("Compare two cities") // H1 tag value
+let isLoading: any = ref(false) // Loading bool
+let search: any = ref(true) // Search bool
+let searched: any = ref(false) // Search done bool
+let error: any = ref("") // Error message value
+let data_weather: any = ref([]); // Weather data Array
 
 
 async function submitForm(query: String) {
     search.value = false
     isLoading.value = true
-    
     
     await axios.get(`http://127.0.0.1:8000/api/${query.value[0].value}/${query.value[1].value}`, {
     })
@@ -38,8 +39,6 @@ async function submitForm(query: String) {
         error.value = err
     })
 }
-
-
 </script>
 
 <template>
@@ -52,13 +51,18 @@ async function submitForm(query: String) {
             {{ error }}
         </div>
             
-        <div class="trip-other" v-if="searched">
-            {{ data_weather?.data[0]?.winner }}
-            {{ data_weather?.data[1] }}
+        <div class="trip-debug" v-if="searched">
+            <!-- {{ data_weather?.data[0]?.winner }} -->
+            <!-- {{ data_weather?.data[1] }} -->
         </div>
 
-        <div class="trip-cwrapper">
-            <TripCard v-if="searched" v-for="(data, i) in data_weather.data[0].cities" :key="i" :weatherData="data" />
+        <div class="trip-winwrapper" v-if="searched">
+            <ConfettiExplosion class="trip-conffeti" :particleCount="100" :particleSize="7" :duration="3000" />
+            <WinnerCard :winnerData="data_weather.data[1]" />
+        </div>
+
+        <div class="trip-cwrapper" v-if="searched">
+            <TripCard v-for="(data, i) in data_weather.data[0].cities" :key="i" :weatherData="data" />
         </div>
 
     </div>
@@ -78,6 +82,20 @@ async function submitForm(query: String) {
         display: flex;
         flex-direction: column;
         gap: 1.5rem;
+    }
+
+    &-winwrapper {
+        align-items: center;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        position: relative;
+
+        .trip-conffeti {
+            position: absolute;
+            top: 10px;
+        }
     }
 }
 </style>
