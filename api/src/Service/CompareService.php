6b89cf83e447146df7d55cc576471b48374e5dab
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-06-13 12:34:51 +0200

    Optimization of the algorythm almost done

--- api/src/Service/CompareService.php
+++ api/src/Service/CompareService.php
@@ -36,15 +36,16 @@ class CompareService
      * 
      * returns a int value into an array.
      */
-    public function assignPoints(int|float $city1offset, int|float $city2offset, array &$compareData, int $points): void
+    public function assignPoints(int|float $cityOffset1, int|float $cityOffset2, array &$compareData, int $points): void
     {
-        // TODO camelCase partout
-        if ($city1offset < $city2offset) {
+        if ($cityOffset1 < $cityOffset2) {
             $compareData['city1score'] += $points;
-        } elseif ($city2offset < $city1offset) {
+        } elseif ($cityOffset1 > $cityOffset2) {
+            $compareData['city2score'] += $points;
+        } elseif ($cityOffset1 = $cityOffset2) {
+            $compareData['city1score'] += $points;
             $compareData['city2score'] += $points;
         }
-        //TODO : Qu'est ce qu'on fait si la différence est égale à 0 ?
     }
 
     /**
@@ -58,10 +59,12 @@ class CompareService
         if ($compareData['city1score'] > $compareData['city2score']) {
             $responseArray['citywinner'] = ['name' => $compareData['city1']['name'], 'country' => $compareData['city1']['sys']['country'], 'score' => $compareData['city1score'], 'tempavg' => $compareData['average']['temp1'], 'humavg' => $compareData['average']['hum1'], 'cloudsavg' => $compareData['average']['clouds1']];
             $responseArray['cityloser'] = ['name' => $compareData['city2']['name'], 'country' => $compareData['city2']['sys']['country'], 'score' => $compareData['city2score'], 'tempavg' => $compareData['average']['temp2'], 'humavg' => $compareData['average']['hum2'], 'cloudsavg' => $compareData['average']['clouds2']];
-        } elseif ($compareData['city2score'] > $compareData['city1score']) {
+        } elseif ($compareData['city1score'] < $compareData['city2score']) {
             $responseArray['citywinner'] = ['name' => $compareData['city2']['name'], 'country' => $compareData['city2']['sys']['country'], 'score' => $compareData['city2score'], 'tempavg' => $compareData['average']['temp2'], 'humavg' => $compareData['average']['hum2'], 'cloudsavg' => $compareData['average']['clouds2']];
             $responseArray['cityloser'] = ['name' => $compareData['city1']['name'], 'country' => $compareData['city1']['sys']['country'], 'score' => $compareData['city1score'], 'tempavg' => $compareData['average']['temp1'], 'humavg' => $compareData['average']['hum1'], 'cloudsavg' => $compareData['average']['clouds1']];
+        } elseif ($compareData['city1score'] = $compareData['city2score']) {
+            // TODO Que fait-on si on a égalité ?
+
         }
-        // TODO Que fait-on si on a égalité ?
     }
 }
